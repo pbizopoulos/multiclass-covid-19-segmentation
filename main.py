68cc9addb0528bb546ed83c93e60e54c2c57e95d
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2023-02-15 22:05:23 +0200

    .

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
 from glob import glob
 from os import environ
-from os.path import exists, isfile, join
+from pathlib import Path
 from shutil import move, rmtree
 from zipfile import ZipFile
 
@@ -29,16 +28,17 @@ def data_generator_1(index_range: range) -> tuple:
     urls = ['https://drive.google.com/uc?id=1SJoMelgRqb0EuqlTuq6dxBWf2j9Kno8S', 'https://drive.google.com/uc?id=1MEqpbpwXjrLrH42DqDygWeSkDq0bi92f', 'https://drive.google.com/uc?id=1zj4N_KV0LBko1VSQ7FPZ38eaEGNU0K6-']
     file_names = ['tr_im.nii.gz', 'tr_mask.nii.gz', 'tr_lungmasks_updated.nii.gz']
     for url, file_name in zip(urls, file_names):
-        if not isfile(join('bin', file_name)):
-            gdown.download(url, join('bin', file_name), quiet=False)
-    images_file_path = join('bin', 'tr_im.nii.gz')
+        file_name_path = Path(f'bin/{file_name}')
+        if not file_name_path.is_file():
+            gdown.download(url, file_name_path.as_posix(), quiet=False)
+    images_file_path = 'bin/tr_im.nii.gz'
     images = nib.load(images_file_path)
     images = images.get_fdata()[..., index_range]
     images = np.moveaxis(images, -1, 0)
-    mask_lesions_file_path = join('bin', 'tr_mask.nii.gz')
+    mask_lesions_file_path = 'bin/tr_mask.nii.gz'
     mask_lesions = nib.load(mask_lesions_file_path)
     mask_lesions = mask_lesions.get_fdata()[..., index_range]
-    mask_lungs_file_path = join('bin', 'tr_lungmasks_updated.nii.gz')
+    mask_lungs_file_path = 'bin/tr_lungmasks_updated.nii.gz'
     mask_lungs = nib.load(mask_lungs_file_path)
     mask_lungs = mask_lungs.get_fdata()[..., index_range]
     mask_lungs[mask_lungs == 2] = 1
@@ -53,19 +53,19 @@ def data_generator_2(index_volume: int) -> tuple:
     urls = ['https://drive.google.com/uc?id=1ruTiKdmqhqdbE9xOEmjQGing76nrTK2m', 'https://drive.google.com/uc?id=1gVuDwFeAGa6jIVX9MeJV5ByIHFpOo5Bp', 'https://drive.google.com/uc?id=1MIp89YhuAKh4as2v_5DUoExgt6-y3AnH']
     file_names = ['rp_im.zip', 'rp_msk.zip', 'rp_lung_msk.zip']
     for url, file_name in zip(urls, file_names):
-        zip_file_path = join('bin', file_name)
-        if not isfile(zip_file_path):
-            gdown.download(url, zip_file_path, quiet=False)
+        zip_file_path = Path(f'bin/{file_name}')
+        if not zip_file_path.is_file():
+            gdown.download(url, zip_file_path.as_posix(), quiet=False)
             with ZipFile(zip_file_path, 'r') as zip_file:
                 zip_file.extractall('bin')
-    image_file_paths = sorted(glob(join('bin', 'rp_im/*.nii.gz')))
+    image_file_paths = sorted(glob('bin/rp_im/*.nii.gz'))
     images = nib.load(image_file_paths[index_volume])
     images = images.get_fdata()
     images = np.moveaxis(images, -1, 0)
-    mask_lesions_file_paths = sorted(glob(join('bin', 'rp_msk/*.nii.gz')))
+    mask_lesions_file_paths = sorted(glob('bin/rp_msk/*.nii.gz'))
     mask_lesions = nib.load(mask_lesions_file_paths[index_volume])
     mask_lesions = mask_lesions.get_fdata()
-    mask_lungs_file_paths = sorted(glob(join('bin', 'rp_lung_msk/*.nii.gz')))
+    mask_lungs_file_paths = sorted(glob('bin/rp_lung_msk/*.nii.gz'))
     mask_lungs = nib.load(mask_lungs_file_paths[index_volume])
     mask_lungs = mask_lungs.get_fdata()
     mask_lungs[mask_lungs == 2] = 1
@@ -79,27 +79,27 @@ def data_generator_3(index_range: range) -> tuple:
     urls = ['https://zenodo.org/record/3757476/files/COVID-19-CT-Seg_20cases.zip?download=1', 'https://zenodo.org/record/3757476/files/Infection_Mask.zip?download=1', 'https://zenodo.org/record/3757476/files/Lung_Mask.zip?download=1']
     file_names = ['COVID-19-CT-Seg_20cases', 'Infection_Mask', 'Lung_Mask']
     for url, file_name in zip(urls, file_names):
-        zip_file_path = join('bin', f'{file_name}.zip')
-        if not isfile(zip_file_path):
+        zip_file_path = Path(f'bin/{file_name}.zip')
+        if not zip_file_path.is_file():
             response = requests.get(url, timeout=60)
-            with open(zip_file_path, 'wb') as file:
+            with zip_file_path.open('wb') as file:
                 file.write(response.content)
             with ZipFile(zip_file_path, 'r') as zip_file:
-                zip_file.extractall(join('bin', file_name))
+                zip_file.extractall(f'bin/{file_name}')
     images = np.array([]).reshape(512, 512, 0)
-    for file_path in glob(join('bin', 'COVID-19-CT-Seg_20cases', '*.nii.gz')):
+    for file_path in glob('bin/COVID-19-CT-Seg_20cases/*.nii.gz'):
         images_ = nib.load(file_path)
         images_ = np.resize(images_.get_fdata(), (512, 512, images_.shape[-1]))
         images = np.concatenate((images, images_), 2)
     images = images[..., index_range]
     mask_lesions = np.array([]).reshape(512, 512, 0)
-    for file_path in glob(join('bin', 'Infection_Mask', '*.nii.gz')):
+    for file_path in glob('bin/Infection_Mask/*.nii.gz'):
         mask_lesions_ = nib.load(file_path)
         mask_lesions_ = np.resize(mask_lesions_.get_fdata(), (512, 512, mask_lesions_.shape[-1]))
         mask_lesions = np.concatenate((mask_lesions, mask_lesions_), 2)
     mask_lesions = mask_lesions[..., index_range]
     mask_lungs = np.array([]).reshape(512, 512, 0)
-    for file_path in glob(join('bin', 'Lung_Mask', '*.nii.gz')):
+    for file_path in glob('bin/Lung_Mask/*.nii.gz'):
         mask_lungs_ = nib.load(file_path)
         mask_lungs_ = np.resize(mask_lungs_.get_fdata(), (512, 512, mask_lungs.shape[-1]))
         mask_lungs = np.concatenate((mask_lungs, mask_lungs_), 2)
@@ -199,14 +199,16 @@ def main() -> None:
     model = get_model(classes_num, img_size)
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics='accuracy')
     model.fit(train_dataset, validation_data=validation_dataset, epochs=epochs_num)
-    if exists(join('bin', 'tfjs')):
-        rmtree(join('bin', 'tfjs'))
-    os.makedirs(join('bin', 'tfjs'), exist_ok=True)
-    tfjs.converters.save_keras_model(model, join('bin', 'tfjs'))
+    tfjs_path = Path('bin/tfjs')
+    if tfjs_path.exists():
+        rmtree(tfjs_path)
+    tfjs_path.mkdir(exist_ok=True)
+    tfjs.converters.save_keras_model(model, tfjs_path)
     if environ['DEBUG'] != '1':
-        if exists('dist'):
-            rmtree('dist')
-        move(join('bin', 'tfjs'), 'dist')
+        dist_path = Path('dist')
+        if dist_path.exists():
+            rmtree(dist_path)
+        move('bin/tfjs', 'dist')
 
 
 def preprocess(images: tf.float32, masks: tf.float32) -> tuple:
